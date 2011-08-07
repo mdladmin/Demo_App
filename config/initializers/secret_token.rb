49679b6c5de9216909498d5661fe1b0d74b24b57
MDLAdmin <mdladmin@gmail.com>
2011-08-07 15:52:21 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4f7ead3910de406bf717de24493395e48ec24bfd00955000fb2fb4cd225d1d618d4294a8c2525ec4187032cfab89ef5b819a1f82faea61689498df976c421828'
